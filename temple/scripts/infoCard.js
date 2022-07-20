4cbb2bfe93d50b1fcb22137b7ffac4d8fd096d78
mictlantecutli <moisesparra82@hotmail.com>
2022-07-19 23:50:11 -0500

    upload new json

--- temple/scripts/infoCard.js
+++ temple/scripts/infoCard.js
@@ -1,5 +1,5 @@
-const requestURL = 'https://mictlantecutli.github.io/wdd230-end-project/jsonfiles/infotemples.json';
-const directoryBox = document.querySelector('.directory_box');
+const requestURL = 'https://mictlantecutli.github.io/wdd230-end-project/jsonFiles/infoTemples.json';
+const directoryBox = document.querySelector('.temple_cards');
 
 
 fetch(requestURL)
@@ -10,30 +10,34 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
 
 
-    const dataBusiness = jsonObject['directoryOfBusiness'];
+    const dataBusiness = jsonObject['directoryTemples'];
     dataBusiness.forEach(displayCompanies);
   });
   
 
-function displayCompanies(company) {
+function displayCompanies(temple) {
   // Create elements to add to the document
-  let directorySection = document.createElement('section');
-  let logoCompany = document.createElement('img');
-  let nameCompany = document.createElement('h2')
-  let addressCompany = document.createElement('p');
+  let templeSection = document.createElement('section');
+ 
+  let nameTemple = document.createElement('h2')
+  let addressTemple = document.createElement('p');
+
   let phoneCompany = document.createElement('p')
   let webCompany = document.createElement('p')
   let levelMember = document.createElement('p')
 
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  logoCompany.setAttribute('src', `images/${company.logoImage}`);
-  logoCompany.setAttribute('alt', `Logo of: ${company.logoImage}`);
-  logoCompany.setAttribute('loading', 'lazy');
+  //logoCompany.setAttribute('src', `images/${company.logoImage}`);
+  //logoCompany.setAttribute('alt', `Logo of: ${company.logoImage}`);
+  //logoCompany.setAttribute('loading', 'lazy');
+
+
 
   // Change the textContent property of the h2, p element to contain the name, address, phone, website and membership of the companies.
-  nameCompany.textContent = `${company.name}`
-  addressCompany.textContent = `${company.address}`
+  nameTemple.textContent = `${temple.name}`
+  addressTemple.textContent = `${temple.address}`
+
   phoneCompany.textContent = `${company.phone}`
   webCompany.textContent = `${company.website}`
   levelMember.textContent = `${company.membership}`
@@ -42,15 +46,16 @@ function displayCompanies(company) {
 
 
   // Add/append the section(company) with img, h2 and four p element.
-  directorySection.appendChild(logoCompany);
-  directorySection.appendChild(nameCompany);
-  directorySection.appendChild(addressCompany);
-  directorySection.appendChild(phoneCompany);
-  directorySection.appendChild(webCompany);
-  directorySection.appendChild(levelMember);
+ 
+  templeSection.appendChild(nameTemple);
+  templeSection.appendChild(addressTemple);
+  s
+  templeSection.appendChild(phoneCompany);
+  templeSection.appendChild(webCompany);
+  templeSection.appendChild(levelMember);
 
 
 
   // Add/append the existing HTML div with the directory_box class with the section(company)
-  document.querySelector('.directory_box').appendChild(directorySection);
+  document.querySelector('.temple_cards').appendChild(directorySection);
 }

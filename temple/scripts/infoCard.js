a1c6e760ae2578382ba88cb2b206c97ac4780720
mictlantecutli <moisesparra82@hotmail.com>
2022-07-20 02:42:50 -0500

    CHANGES IN JSON FILE

--- temple/scripts/infoCard.js
+++ temple/scripts/infoCard.js
@@ -10,21 +10,31 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
 
 
-    const dataBusiness = jsonObject['directoryTemples'];
-    dataBusiness.forEach(displayCompanies);
+    const dataTemples = jsonObject['directoryTemples'];
+    dataTemples.forEach(displayTemples);
   });
   
 
-function displayCompanies(temple) {
+function displayTemples(temple) {
   // Create elements to add to the document
   let templeSection = document.createElement('section');
  
-  let nameTemple = document.createElement('h2')
+  let nameTemple = document.createElement('h2');
   let addressTemple = document.createElement('p');
+  let phoneTemple = document.createElement('p');
+  let emailTemple = document.createElement('p');
+  let servicesTemple = document.createElement('ul');
+  let divList = document.createElement('div');
+  let headingList = document.createElement('h3');
 
-  let phoneCompany = document.createElement('p')
-  let webCompany = document.createElement('p')
-  let levelMember = document.createElement('p')
+  let containerHistory = document.createElement('ul');
+  let divHistory = document.createElement('div');
+  let headingHistory = document.createElement('h3');
+
+  let headingOrdinances = document.createElement('h3');
+  let textOrdinances = document.createElement('p')
+  let divOrdinances = document.createElement('div');
+
 
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
@@ -37,10 +47,49 @@ function displayCompanies(temple) {
   // Change the textContent property of the h2, p element to contain the name, address, phone, website and membership of the companies.
   nameTemple.textContent = `${temple.name}`
   addressTemple.textContent = `${temple.address}`
+  phoneTemple.textContent = `${temple.phone}`
+  emailTemple.textContent = `${temple.email}`
+  headingList.textContent = "SERVICES"
+  headingHistory.textContent = "HISTORY"
+  headingOrdinances.textContent = "ORDINANCE SCHELUDE"
+  textOrdinances.textContent = `${temple.ordinance}`
+  
+  //I iterate in the list of services of every temple
+  temple.services.forEach( service =>{
+    
+    let listService = document.createElement('li');
 
-  phoneCompany.textContent = `${company.phone}`
-  webCompany.textContent = `${company.website}`
-  levelMember.textContent = `${company.membership}`
+    listService.textContent = service;
+
+    servicesTemple.appendChild(listService);
+
+  })
+
+  //I iterate in the list of the history of every temple
+  temple.history.forEach( milestone =>{   
+    let listMilestone = document.createElement('li');
+
+    listMilestone.textContent = milestone;
+
+    containerHistory.appendChild(listMilestone);
+
+  })
+
+
+
+//HERE I added the h3 and the list in the div element
+  divList.appendChild(headingList);
+  divList.appendChild(servicesTemple);
+
+  divHistory.appendChild(headingHistory);
+  divHistory.appendChild(containerHistory);
+
+  divOrdinances.appendChild(headingOrdinances);
+  divOrdinances.appendChild(textOrdinances);
+
+
+
+
 
 
 
@@ -49,13 +98,14 @@ function displayCompanies(temple) {
  
   templeSection.appendChild(nameTemple);
   templeSection.appendChild(addressTemple);
-  s
-  templeSection.appendChild(phoneCompany);
-  templeSection.appendChild(webCompany);
-  templeSection.appendChild(levelMember);
+  templeSection.appendChild(phoneTemple);
+  templeSection.appendChild(emailTemple);
+  templeSection.appendChild(divList);
+  templeSection.appendChild(divHistory);
+  templeSection.appendChild(divOrdinances);
 
 
 
   // Add/append the existing HTML div with the directory_box class with the section(company)
-  document.querySelector('.temple_cards').appendChild(directorySection);
+  document.querySelector('.temple_cards').appendChild(templeSection);
 }
